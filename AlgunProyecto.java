51ed667c61f43b9afbebe0105187bd8e89ac17eb
AnaP11 <anachantal.poncealcaniz@educa.madrid.org>
2021-11-05 21:14:07 +0100

    Segundo cambio

--- AlgunProyecto.java
+++ AlgunProyecto.java
@@ -1,5 +1,6 @@
 public class AlgunProyecto {
     public static void main(String[] args) {
         System.out.println("Esto se va a imprimir.");
+        System.out.println("Segundo cambio, desde la rama principal o master");
     }
 }
